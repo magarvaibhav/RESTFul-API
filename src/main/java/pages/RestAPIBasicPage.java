6a28a13a8cae0e5f33579474731ffd071a53af8a
magarrajesh99 <magarrajesh99@gmail.com>
2021-08-27 09:59:13 +0530

    commit simple reading response from server

--- src/main/java/pages/RestAPIBasicPage.java
+++ src/main/java/pages/RestAPIBasicPage.java
@@ -1,5 +1,57 @@
 package pages;
 
+import io.restassured.RestAssured;
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
 public class RestAPIBasicPage {
 
+	RequestSpecification objRequestSpecification =null;
+	Response response=null;
+	
+	public Response getResponseFromServer(String sCity)
+	{
+		//get requestSpecification from restAssued using given method
+		objRequestSpecification = RestAssured.given();
+		
+		//specify method name and URL and get response from server
+		response = objRequestSpecification.request(Method.GET,"/"+sCity);
+	
+		//return
+		return response;
+	}
+	
+	public String getResponseBodyFromServer()
+	{
+		return response.getBody().asString();
+	}
+	
+	public int getStatusCodeFromServer()
+	{
+		return response.getStatusCode();
+	}
+
+	public String getStatusLineFromServer() {
+		return response.getStatusLine();
+
+	}
+	
+	public void printAllHeaders() {
+		Headers objHeaders = response.getHeaders();
+		
+		for(Header href : objHeaders)
+		{
+			System.out.println("Name : "+href.getName()+" Value : "+href.getValue());
+		}
+
+	}
+	
+	public String printHeaderValue(String headerName)
+	{
+		return response.getHeader(headerName);
+	}
+	
 }
--- src/test/java/tests/RestAPIBasicTest.java
+++ src/test/java/tests/RestAPIBasicTest.java
@@ -1,29 +1,58 @@
 package tests;
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 import io.restassured.RestAssured;
-import io.restassured.http.Method;
 import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
+import pages.RestAPIBasicPage;
 
 public class RestAPIBasicTest {
 	
-	public static void main(String arg[])
+	String sCity="Pune";
+	RestAPIBasicPage objRestAPIBasicPage=null;
+	int OK =200;
+	String STATUSLINE="HTTP/1.1 200 OK";
+	
+	@BeforeMethod()
+	void setUpRestAssured()
 	{
-		String sCity="Pune";
-		//set BaseURL for RESTful web service
 		RestAssured.baseURI="https://demoqa.com/utilities/weather/city";
-		
-		//get requestSpecification from restAssued using given method
-		RequestSpecification objRequestSpecification = RestAssured.given();
-		
-		//specify method name and URL and get response from server
-		Response response = objRequestSpecification.request(Method.GET,"/"+sCity);
-		
-		//get all data of response and convert in string and print
-		String responseBody = response.getBody().asString();
-		System.out.println("Response Body is =>  " + responseBody);
-
-		
+		objRestAPIBasicPage = new RestAPIBasicPage();
+	}
+	
+	@Test()
+	void testResponseFromServer()
+	{
+		objRestAPIBasicPage.getResponseFromServer(sCity);
+		Assert.assertTrue(!(objRestAPIBasicPage.getResponseBodyFromServer().isEmpty()), sCity+ " city dont have a data in server");
+	}
+	
+	@Test()
+	void testStatusCodeOfResponse()
+	{
+		objRestAPIBasicPage.getResponseFromServer(sCity);
+		Assert.assertEquals(objRestAPIBasicPage.getStatusCodeFromServer(), OK);
+	}
+	
+	
+	@Test()
+	void testStatusLineOfResponse()
+	{
+		objRestAPIBasicPage.getResponseFromServer(sCity);
+		System.out.println(objRestAPIBasicPage.getStatusLineFromServer());
+		Assert.assertEquals(objRestAPIBasicPage.getStatusLineFromServer(), STATUSLINE);
 	}
 
+	@Test()
+	void testPrintHeaders()
+	{
+		objRestAPIBasicPage.getResponseFromServer(sCity);
+		objRestAPIBasicPage.printAllHeaders();
+		System.out.println(objRestAPIBasicPage.printHeaderValue("Content-Type"));
+		System.out.println(objRestAPIBasicPage.printHeaderValue("Content-Length"));
+		System.out.println(objRestAPIBasicPage.printHeaderValue("Connection"));
+		System.out.println(objRestAPIBasicPage.printHeaderValue("Server"));
+	}
 }
